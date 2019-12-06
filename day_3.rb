4e3fcc9dedef09e9d64a365f39ade0c9aca3483c
RW <rinze.watanabe.translation@gmail.com>
2019-12-06 13:18:26 +1000

    Update

--- day_3.rb
+++ day_3.rb
@@ -1,8 +1,71 @@
-class Pointer
+require 'set'
 
-  attr_accessor :x,; y
 
-  def initialize(*args)
-    @x, @y = args
+class Wire
+  attr_accessor :points, :current
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @points = Set.new
+    @current = [0, 0]
+    @count = {}
+  end
+
+  def add(distance)
+    case distance[0]
+    when 'U'
+      distance[1..distance.length].to_i.times do
+        @current[1] += 1
+        @points.add(current.dup)
+      end
+
+
+    when 'R'
+      distance[1..distance.length].to_i.times do
+        @current[0] += 1
+        points.add(current.dup)
+      end
+
+
+    when 'D'
+      distance[1..distance.length].to_i.times do
+        @current[1] -= 1
+        @points.add(@current.dup)
+      end
+
+
+    when 'L'
+      distance[1..distance.length].to_i.times do
+        @current[0] -= 1
+        @points.add(@current.dup)
+      end
+    end
   end
 end
+
+
+wire_a = Wire.new(0, 0)
+wire_b = Wire.new(0, 0)
+
+File.readlines('inputs/day_3.txt').each_with_index do |line, i|
+  line.split(',').each do |distance|
+    if i.zero?
+      wire_a.add(distance)
+
+    else
+      wire_b.add(distance)
+    end
+  end
+end
+
+
+intersection = wire_b.points.intersection(wire_a.points)
+minimum = intersection.first[0].abs + intersection.first[1].abs
+
+intersection.each do |i|
+  distance = i[0].abs + i[1].abs
+  minimum = distance if minimum > distance
+end
+
+p minimum
