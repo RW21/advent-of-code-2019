3d4a44ac404a7942ac9ef4f550e8d20584f5e848
RW21 <togotonext@gmail.com>
2019-12-09 12:02:06 +1000

    Update

---
+++ day_6.py
@@ -0,0 +1,37 @@
+def get_indirect_orbits(orbits, target):
+    count = 0
+    while target in orbits:
+        target = orbits[target]
+        count += 1
+
+    return count - 1
+
+
+def get_total_orbits(orbits, target):
+    count = 0
+    while target in orbits:
+        target = orbits[target]
+        count += 1
+
+    return count
+
+
+with open('inputs/day_6.txt') as f:
+    lines = f.readlines()
+
+orbits = {}
+
+for line in lines:
+    line = line.split(')')
+
+    child = line[1][:3]
+    parent = line[0]
+
+    orbits[child] = parent
+
+
+count = 0
+for orbit in orbits.keys():
+    count += get_total_orbits(orbits, orbit)
+
+print(count)
